43762ca1ed21fdc6df48e210368a91d76f7a44e4
François Chabot <fchabot@skynet.be>
2021-02-03 11:36:35 +0100

    Clean Slate

---
+++ src/Be.Stateless.BizTalk.Factory.Logging/Environment.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2021 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Be.Stateless.Diagnostics;
+
+namespace Be.Stateless.BizTalk
+{
+	public static class Environment
+	{
+		[SuppressMessage("ReSharper", "CommentTypo")]
+		public static string GetHostName()
+		{
+			// e.g. "C:\Program Files (x86)\Microsoft BizTalk Server\BTSNTSvc64.exe" -group "BizTalk Group" -name "BizTalkServerApplication" -btsapp "{EA0E4664-2493-40C7-B7C9-CD33C671F0D5}"
+			var args = System.Environment.GetCommandLineArgs();
+			var index = Array.IndexOf(args, "-name", 1);
+			if (index > 0) return args[index + 1];
+
+			EventLog.WriteWarning($"Failed to parse BizTalk Server Host Name from command line: {System.Environment.CommandLine}.");
+			return null;
+		}
+	}
+}
--- src/Be.Stateless.BizTalk.Factory.Runtime.Deployment/Manifest.ps1
+++ src/Be.Stateless.BizTalk.Factory.Runtime.Deployment/Manifest.ps1
@@ -1,17 +1,50 @@
-﻿param(
-   [Parameter(Mandatory = $true)]
-   [ValidateSet('DEV', 'BLD', 'ACC', 'PRE', 'PRD')]
-   $TargetEnvironment
+﻿#region Copyright & License
+
+# Copyright © 2019 - 2022 François Chabot
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#endregion
+
+#Requires -Modules @{ ModuleName = 'BizTalk.Deployment' ; ModuleVersion = '2.1.0.0' ; MaximumVersion = '2.2.0.0' ; GUID = '533b5f59-49ce-4f51-a293-cb78f5cf81b5' }
+
+[CmdletBinding()]
+[OutputType([HashTable])]
+param(
+   [Parameter(Mandatory = $false)]
+   [ValidateNotNullOrEmpty()]
+   [ValidateScript( { Test-Path -Path $_ } )]
+   [string]
+   $LogDirectory = 'C:\Files\Logs',
+
+   [Parameter(Mandatory = $false)]
+   [ValidateSet('ALL', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL', 'OFF')]
+   [string]
+   $LogLevel = 'DEBUG'
 )
 
-LibraryManifest -Name BizTalk.Factory.Runtime -Description 'Comprehensive BizTalk Server Runtime' -Build {
-   Assembly -Path (Get-ResourceItem -Name log4net,
-      Be.Stateless.Common,
-      Be.Stateless.Extensions,
+Set-StrictMode -Version Latest
+
+LibraryManifest -Name BizTalk.Factory.Runtime -Description 'BizTalk.Factory''s Comprehensive Set of BizTalk Server Runtime Components.' -Build {
+   Assembly -Path (Get-ResourceItem -Name log4net) -InstallReference BizTalk.Factory.Runtime
+   Assembly -Path (Get-ResourceItem -Name Be.Stateless.Extensions,
+      Be.Stateless.Reflection,
+      Be.Stateless.Runtime,
+      Be.Stateless.Security,
       Be.Stateless.Stream,
       Be.Stateless.Xml,
       Be.Stateless.BizTalk.Abstractions,
-      Be.Stateless.BizTalk.Activity.Tracking,
+      Be.Stateless.BizTalk.Factory.Logging,
       Be.Stateless.BizTalk.Messaging,
       Be.Stateless.BizTalk.Pipeline.MicroComponents,
       Be.Stateless.BizTalk.ServiceModel,
@@ -21,4 +54,20 @@ LibraryManifest -Name BizTalk.Factory.Runtime -Description 'Comprehensive BizTal
       Be.Stateless.BizTalk.XLang,
       Be.Stateless.BizTalk.Xml
    )
+   EventLogSource -Name 'BizTalk.Factory' -LogName Application
+   File -Path (Get-ResourceItem -Name BTSNTSvc.exe.log4net -Extension .config) `
+      -Destination (Join-Path $env:BTSINSTALLPATH BTSNTSvc.exe.log4net.config), (Join-Path $env:BTSINSTALLPATH BTSNTSvc64.exe.log4net.config)
+   XmlConfigurationAction -Path (Join-Path $env:BTSINSTALLPATH BTSNTSvc.exe.log4net.config) `
+      -Update /log4net/appender[@name="'RollingFileAppender'"]/file `
+      -Attribute @{ value = $(Join-Path $LogDirectory '%property{btshost}.log') }
+   XmlConfigurationAction -Path (Join-Path $env:BTSINSTALLPATH BTSNTSvc64.exe.log4net.config) `
+      -Update /log4net/appender[@name="'RollingFileAppender'"]/file `
+      -Attribute @{ value = $(Join-Path $LogDirectory '%property{btshost}.log') }
+   XmlConfigurationAction -Path (Join-Path $env:BTSINSTALLPATH BTSNTSvc.exe.log4net.config) `
+      -Update /log4net/root/level `
+      -Attribute @{ value = $LogLevel }
+   XmlConfigurationAction -Path (Join-Path $env:BTSINSTALLPATH BTSNTSvc64.exe.log4net.config) `
+      -Update /log4net/root/level `
+      -Attribute @{ value = $LogLevel }
+   XmlConfiguration -Path (Get-ResourceItem -Name BTSNTSvc.exe, machine -Extension .config)
 }
